78d925a23df85dd6a755745e29292e86acef5ffa
Djames Eddine <djames.d@outlook.com>
2025-02-09 18:40:44 +0100

    working on the tests

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -3,15 +3,13 @@ import mlflow.sklearn
 
 
 # Define the model names in MLflow
-MODEL_NAMES = ["Naive Bayes", "Logistic Regression", "lstm_model"]
+#add lstm after logistic regression in the model names
+MODEL_NAMES = ["Naive Bayes", "Logistic Regression"]
 
 @pytest.mark.parametrize("model_name", MODEL_NAMES)
 def test_load_models(model_name):
-    """
-    Test loading trained models from MLflow.
-    """
+    
     try:
-        # Load the latest version of the model from MLflow Model Registry
         model = mlflow.sklearn.load_model(f"models:/{model_name}/latest")
         assert model is not None, f"Failed to load model: {model_name}"
         print(f"✅ Successfully loaded {model_name} model from MLflow.")
@@ -19,7 +17,11 @@ def test_load_models(model_name):
     except Exception as e:
         pytest.fail(f"❌ Error loading {model_name}: {e}")
 
+
+
 if __name__ == "__main__":
-    pytest.main()
+    pytest.main(["-v", "--tb=short"], plugins=[CaptureOutput(f)])
+    with open("test_results.txt", "w") as f:
+    pytest.main(["-v", "--tb=short"], stdout=f)
 
     
